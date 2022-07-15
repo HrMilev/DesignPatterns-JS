5203548840f37de8c8d69bed41c4667e9494e707
HrMilev <hr.milev@yahoo.bg>
2022-07-15 21:45:24 +0300

    Add Factory method pattern

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import { proxy } from './Proxy/proxy.js';
 import { decorator } from './Decorator/decorator.js';
 import { composite } from './Composite/composite.js';
 import { adapter } from './Adapter/adapter.js';
+import { factoryMethod } from './FactoryMethod/factoryMethod.js';
 
 interface PatternWindow extends Window {
     proxyPattern(name: string) : void
     decoratorPattern(from: string, text: string, instructions: string[]) : void
     compositePattern() : void
     adapterPattern(): void
+    factoryMethodPattern(type: number, name: string): void
 }
 
 declare let window: PatternWindow;
@@ -19,3 +21,5 @@ window.decoratorPattern = decorator; // try with 'John', 'Hello!', ['E', 'B', 'E
 window.compositePattern = composite;
 
 window.adapterPattern = adapter;
+
+window.factoryMethodPattern = factoryMethod; // try with 0 (cheap), 'John'
